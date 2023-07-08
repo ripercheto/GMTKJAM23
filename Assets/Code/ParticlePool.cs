a011192097b66600581c205ae35e35c1b549dd63
ripercheto <pavel.stoimenov@abv.bg>
2023-07-08 21:26:04 +0200

    particle pool

--- Assets/Code/ParticlePool.cs
+++ Assets/Code/ParticlePool.cs
@@ -3,12 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ParticleType
+{
+    Death,
+    Spawn
+}
+
+
 public class ParticlePool : PrefabPool<ParticleSystem>
 {
-    public static ParticlePool instance;
+    public ParticleType type;
+    private static ParticlePool spawn;
+    private static ParticlePool death;
+
+    public static ParticlePool Get(ParticleType type)
+    {
+        switch (type)
+        {
+            case ParticleType.Death:
+                return death;
+            case ParticleType.Spawn:
+                return spawn;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+    }
 
     private void Awake()
     {
-        instance = this;
+        switch (type)
+        {
+            case ParticleType.Death:
+                death= this;
+                break;
+            case ParticleType.Spawn:
+                spawn = this;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }

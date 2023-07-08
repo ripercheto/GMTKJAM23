8470f9b2611b9456c28cf6b5fe10959bf74100cd
ripercheto <pavel.stoimenov@abv.bg>
2023-07-08 17:32:09 +0200

    [Axe] Lemmie axe yo ua question

--- Assets/Code/WeaponData.cs
+++ Assets/Code/WeaponData.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class WeaponData : BaseItemData
@@ -8,29 +9,38 @@ public class WeaponData : BaseItemData
     public Weapon weaponPrefab;
 
     public float damage = 50f;
+    public float cooldown = 1f;
+    public float duration = 0.5f;
+    [Space, Min(0)]
+    public float moveAmount = 1f;
+    [HideIf(nameof(moveAmount), 0)]
+    public AnimationCurve moveCurve;
+
+    [Space]
+    public float attackAngle = 180f;
+    [Space]
     public float durability = 50f;
     public float durabilityPerHit = 5f;
-    public float duration = 0.5f;
-    public float attackAngle = 180f;
-    public float cooldown = 1f;
 
     private Weapon weapon;
-    
+
     public IEnumerator PerformAttack(Rigidbody body, Vector3 dir, Action<bool> onDone)
     {
+        dir.Normalize();
         var halfAngle = attackAngle * 0.5f;
         var startRotation = Quaternion.LookRotation(dir, Vector3.up);
 
         weapon = Instantiate(weaponPrefab, body.position, startRotation * Quaternion.AngleAxis(-halfAngle, Vector3.up));
         weapon.actionOnEnter = (x) => x.TakeDamage(damage);
-
+        var endPos = dir * moveAmount;
         var t = 0f;
         while (t < 1f)
         {
             t += Time.fixedDeltaTime / duration;
             yield return new WaitForFixedUpdate();
             var angle = Mathf.Lerp(-halfAngle, halfAngle, t);
-            weapon.body.MovePosition(body.position);
+            var pos = Vector3.Lerp(Vector3.zero, endPos, moveCurve.Evaluate(t));
+            weapon.body.MovePosition(body.position + pos);
             weapon.body.MoveRotation(startRotation * Quaternion.AngleAxis(angle, Vector3.up));
         }
 

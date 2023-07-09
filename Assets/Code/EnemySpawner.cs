3594f9d944146fff413ef8d484328b77492da6b8
ripercheto <pavel.stoimenov@abv.bg>
2023-07-09 12:37:21 +0200

    [EnemySpawner] cooldown decrease per spawn

--- Assets/Code/EnemySpawner.cs
+++ Assets/Code/EnemySpawner.cs
@@ -8,12 +8,16 @@ public class EnemySpawner : MonoBehaviour
     public EnemyType type = EnemyType.Bat;
     public MinMaxFloat spawnDelay;
     public MinMaxFloat spawnCooldown;
+    public float minCooldown = 3;
+    public float cooldownDecreasePerSpawn = 1;
+
     public MinMaxInt spawnAmount;
     public float spawnRange = 5;
     public AudioSource spawnSound;
 
     private float spawnTime;
     private EnemyPool pool;
+    private float cooldownDecrease;
 
     private void Awake()
     {
@@ -50,6 +54,8 @@ public class EnemySpawner : MonoBehaviour
             var random = Random.insideUnitCircle * spawnRange;
             pool.Activate(transform.position + random.To3D());
         }
+
+        cooldownDecrease += cooldownDecreasePerSpawn;
     }
 
     private void OnDrawGizmosSelected()
@@ -59,6 +65,7 @@ public class EnemySpawner : MonoBehaviour
 
     private void SetSpawnTime()
     {
-        spawnTime = Time.time + spawnCooldown.GetRandom();
+        var cooldown = Mathf.Max(minCooldown, spawnCooldown.GetRandom() - cooldownDecrease);
+        spawnTime = Time.time + cooldown;
     }
 }
